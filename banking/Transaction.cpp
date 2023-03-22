f115891f3d8e7cf8ae5068b8c339acd9bf4e11fa
Egor Devyatkin <125737299+ledibonibell@users.noreply.github.com>
2023-03-22 19:23:34 +0300

    Update Transaction.cpp

--- banking/Transaction.cpp
+++ banking/Transaction.cpp
@@ -52,9 +52,9 @@ void Transaction::Debit(Account& accout, int sum) {
   assert(sum > 0);
   if (accout.GetBalance() > sum) {
     accout.ChangeBalance(-sum);
-    //return true;
+    return true;
   }
-  //return false;
+  return false;
 }
 
 void Transaction::SaveToDataBase(Account& from, Account& to, int sum) {
